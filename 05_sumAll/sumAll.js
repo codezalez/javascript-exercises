9029135001dd01def83141187dfd0be9145b9f02
codezalez <egonzalezla95@gmail.com>
2023-12-12 19:02:00 -0300

    Assignment 05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
+    let sum = 0
+    const increment = start < finish ? 1 : -1
 
+    if (start < 0 || finish < 0) return 'ERROR'
+    if (!Number.isInteger(start) || !Number.isInteger(finish)) return 'ERROR' 
+
+    for (let i = start; start < finish ? i <= finish : i >= finish; i += increment) {
+        sum += i
+    }
+
+    return sum
+
+        //My first solution for the sum
+    // if (start < finish) {
+    //     for (let i = start ; i <= finish; i++) {
+    //         sum += i
+    //     }
+    //     return sum
+    // } else {
+    //     for (let i = start; i >= finish; i--) {
+    //         sum += i
+    //     }
+    //     return sum
+    // }
 };
 
 // Do not edit below this line
